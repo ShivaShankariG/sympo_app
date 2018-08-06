8e98a53f856ef2a4137202611aa82dca8e8639d3
ShivaShankariG <shivguru06@gmail.com>
2018-08-06 22:29:32 +0530

    Basic app

---
+++ data.js
@@ -0,0 +1,47 @@
+var type = "";
+var marks = 50;
+var easy = [
+    [
+        "this is easy qn1",
+        "easy ans1"
+    ],
+    [
+        "this is easy qn2",
+        "easy ans2"
+    ],
+    [
+        "this is easy qn3",
+        "easy ans3"
+    ]
+]
+
+var medium = [
+    [
+        "this is medium  qn1",
+        "medium ans1"
+    ],
+    [
+        "this is medium qn2",
+        "medium ans2"
+    ],
+    [
+        "this is medium qn3",
+        "medium ans3"
+    ]
+]
+
+var hard = [
+    [
+        "this is hard qn1",
+        "hard ans1"
+    ],
+    [
+        "this is hard qn2",
+        "hard ans2"
+    ],
+    [
+        "this is medium qn3",
+        "medium ans3"
+    ]
+]
+
